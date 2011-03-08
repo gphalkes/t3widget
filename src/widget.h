19f9559e973381881edcb0ae1bb4327dbd62f463
Gertjan Halkes <hg@ghalkes.nl>
2011-03-08 14:57:23 +0100

    Added top level header

---
+++ src/widget.h
@@ -0,0 +1,42 @@
+/* Copyright (C) 2010 G.P. Halkes
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License version 3, as
+   published by the Free Software Foundation.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef T3_WIDGET_H
+#define T3_WIDGET_H
+
+#include "main.h"
+
+#include "dialogs/dialogs.h"
+#include "dialogs/filedialog.h"
+#include "dialogs/mainwindow.h"
+#include "dialogs/menupanel.h"
+#include "dialogs/messagedialog.h"
+
+#include "widgets/widgets.h"
+#include "widgets/bullet.h"
+#include "widgets/button.h"
+#include "widgets/checkbox.h"
+#include "widgets/dummywidget.h"
+#include "widgets/editwindow.h"
+#include "widgets/filepane.h"
+#include "widgets/frame.h"
+#include "widgets/label.h"
+#include "widgets/listpane.h"
+#include "widgets/menu.h"
+#include "widgets/menuitem.h"
+#include "widgets/multiwidget.h"
+#include "widgets/scrollbar.h"
+#include "widgets/smartlabel.h"
+#include "widgets/textfield.h"
+
+#endif
