b391f32435ca1474e4245e885aad9ec60023c06f
Gertjan Halkes <hg@ghalkes.nl>
2012-09-29 20:19:53 +0200

    Added expander_t widget

---
+++ src/widgets/expander.cc
@@ -0,0 +1,146 @@
+/* Copyright (C) 2012 G.P. Halkes
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License version 3, as
+   published by the Free Software Foundation.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include "widgets/expander.h"
+#include "colorscheme.h"
+
+using namespace std;
+namespace t3_widget {
+
+expander_t::expander_t(const char *_text) : is_expanded(false), label(_text), child(NULL), full_height(2) {
+	init_unbacked_window(1, label.get_width() + 2);
+	label.set_position(0, 2);
+	if ((symbol_window = t3_win_new(window, 1, 2, 0, 0, 0)) == NULL)
+		throw bad_alloc();
+}
+
+void expander_t::set_child(widget_t *_child) {
+	/* FIXME: connect to move_focus_XXX events. */
+	child = _child;
+	set_widget_parent(child);
+	child->set_anchor(this, 0);
+	child->set_position(1, 0);
+	child->set_size(full_height - 1, t3_win_get_width(window));
+}
+
+bool expander_t::process_key(key_t key) {
+	if (focus == FOCUS_SELF) {
+		if (child != NULL && (key == '\t' || key == EKEY_DOWN)) {
+			focus = FOCUS_CHILD;
+			return true;
+		} else if (key == ' ' || key == EKEY_NL) {
+			if (!is_expanded && child != NULL) {
+				t3_win_resize(window, full_height, t3_win_get_width(window));
+				is_expanded = true;
+				redraw = true;
+				child->show();
+				if (child->accepts_focus()) {
+					focus = FOCUS_CHILD;
+					child->set_focus(true);
+				}
+			} else if (is_expanded) {
+				if (child != NULL)
+					child->hide();
+				t3_win_resize(window, 1, t3_win_get_width(window));
+				is_expanded = false;
+				redraw = true;
+			}
+			return true;
+		}
+	} else if (focus == FOCUS_CHILD) {
+		bool result = child->process_key(key);
+		if (!result && key == (EKEY_SHIFT | '\t')) {
+			focus = FOCUS_SELF;
+			result = true;
+		}
+		return result;
+	}
+	return false;
+}
+
+void expander_t::update_contents(void) {
+	if (is_expanded && child != NULL)
+		child->update_contents();
+	if (!redraw)
+		return;
+
+	label.update_contents();
+	t3_win_set_paint(symbol_window, 0, 0);
+	t3_win_set_default_attrs(symbol_window, T3_ATTR_ACS | (focus == FOCUS_SELF ? attributes.dialog_selected : attributes.dialog));
+	t3_win_addch(symbol_window, is_expanded ? T3_ACS_DARROW : T3_ACS_RARROW, 0);
+}
+
+void expander_t::set_focus(bool _focus) {
+	/* FIXME: can we somehow determine where the focus came from, such that when
+	   we shift-tab into this, we can set the focus to the child iso ourselves. */
+	if (_focus) {
+		focus = FOCUS_SELF;
+	} else {
+		if (focus == FOCUS_CHILD && child != NULL)
+			child->set_focus(false);
+		focus = FOCUS_NONE;
+	}
+	redraw = true;
+}
+
+bool expander_t::set_size(optint height, optint width) {
+	bool result = true;
+	if (height.is_valid() && height > 1) {
+		full_height = height;
+	}
+
+	if (child != NULL)
+		result &= child->set_size(full_height - 1, width);
+
+	if (!width.is_valid())
+		width = t3_win_get_width(window);
+	if (is_expanded)
+		result &= t3_win_resize(window, full_height, width);
+	else
+		result &= t3_win_resize(window, 1, width);
+	return result;
+}
+
+bool expander_t::is_hotkey(key_t key) {
+	return label.is_hotkey(key);
+}
+
+void expander_t::set_enabled(bool enable) {
+	enabled = enable;
+	if (child != NULL)
+		child->set_enabled(enable);
+}
+
+void expander_t::force_redraw(void) {
+	if (child != NULL)
+		child->force_redraw();
+}
+
+void expander_t::focus_set(window_component_t *target) {
+	container_t *container;
+	if (target == child)
+		child->set_focus(true);
+	container = dynamic_cast<container_t *>((widget_t *) child);
+	if (container != NULL)
+		container->focus_set(target);
+}
+
+bool expander_t::is_child(window_component_t *component) {
+	container_t *container;
+	if (component == child)
+		return true;
+	container = dynamic_cast<container_t *>((widget_t *) child);
+	return container != NULL && container->is_child(component);
+}
+
+}; // namespace
---
+++ src/widgets/expander.h
@@ -0,0 +1,58 @@
+/* Copyright (C) 2012 G.P. Halkes
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License version 3, as
+   published by the Free Software Foundation.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef T3_WIDGET_FRAME_H
+#define T3_WIDGET_FRAME_H
+
+#include <t3widget/widgets/widget.h>
+#include <t3widget/widgets/smartlabel.h>
+
+namespace t3_widget {
+
+/** A widget showing an expander, which allows hiding another widget. */
+class T3_WIDGET_API expander_t : public widget_t, public container_t {
+	private:
+		enum {
+			FOCUS_NONE,
+			FOCUS_SELF,
+			FOCUS_CHILD
+		} focus;
+		bool is_expanded;
+		smart_label_t label;
+		cleanup_t3_window_ptr symbol_window;
+		cleanup_ptr<widget_t>::t child; /**< The widget to enclose. */
+		int full_height;
+
+	public:
+		/** Create a new expander_t.
+		    @param _text The text for the smart_label_t shown next to the expander symbol.
+		*/
+		expander_t(const char *_text);
+		/** Set the child widget. */
+		void set_child(widget_t *_child);
+
+		virtual bool process_key(key_t key);
+		virtual void update_contents(void);
+		virtual void set_focus(bool _focus);
+		virtual bool set_size(optint height, optint width);
+		virtual bool is_hotkey(key_t key);
+		virtual void set_enabled(bool enable);
+		virtual void force_redraw(void);
+		virtual void focus_set(window_component_t *target);
+		virtual bool is_child(window_component_t *component);
+
+	T3_WIDGET_SIGNAL(expanded, void, bool);
+};
+
+}; // namespace
+#endif
