3a77f7b4bc482ba65e162d3a9068f165dcc13104
Gertjan Halkes <git@ghalkes.nl>
2013-03-05 18:17:35 +0100

    Update testsuite to correctly require version 2 of the termdebug script Try to prime the FS cache with the dynamic libraries to reduce testsuite failures

--- testsuite/runtest.sh
+++ testsuite/runtest.sh
@@ -18,6 +18,8 @@ build_test
 unset PRINT_SUBTEST
 [ "`ls $TEST/recording* | wc -l`" -gt 1 ] && PRINT_SUBTEST=1
 
+# Try to prime the FS cache
+ldd test > /dev/null
 
 for RECORDING in $TEST/recording* ; do
 	SUBTEST="${RECORDING##*recording}"
